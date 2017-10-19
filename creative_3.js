e5263a0d72e594e067f985eef3ee3dff358217aa
Alec Ostheimer <alecostheimer94@gmail.com>
2017-10-19 22:54:22 +0000

    dhe

--- creative_3.js
+++ creative_3.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 app.controller('mainCtrl', function($scope) {
    $scope.cymbal1 = function(){
-	var audio = new Audio('cymbal.WAV');
+	var audio = 
 	audio.play(); 
     };
 });
