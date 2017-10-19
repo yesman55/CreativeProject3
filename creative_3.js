d74aa1b9d5e40511337968fe70f1be1df34a3c11
yesman55 <jsi072795@yahoo.com>
2017-10-19 00:45:48 +0000

    modified:   creative_3.js 	modified:   index.html

--- creative_3.js
+++ creative_3.js
@@ -1,7 +1,8 @@
 var app = angular.module('myApp', []);
 app.controller('mainCtrl', function($scope) {
    $scope.cymbal1 = function(){
-	var audio = new Audio('cymbal.WAV');
-	audio.play(); 
+	var audio = document.getElementById("cymbal1audio");
+    audio.load();
+    audio.play(); 
     };
 });
