73ef7055a5fd35434f1e46abb5f1c48751db21af
Alec Ostheimer <alecostheimer94@gmail.com>
2017-10-19 00:12:37 +0000

    dead

--- creative_3.js
+++ creative_3.js
@@ -1,5 +1,7 @@
 var app = angular.module('myApp', []);
 app.controller('mainCtrl', function($scope) {
-   $scope.symbol1 = 
+   $scope.cymbal1 = function(){
+	var audio = new Audio('cymbal.WAV');
+	audio.play(); 
     };
 });
