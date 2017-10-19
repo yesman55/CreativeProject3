2fc6d50bd7ac7810a8ae25256f019c69997c40ee
yesman55 <jsi072795@yahoo.com>
2017-10-19 00:57:20 +0000

    modified:   creative_3.js 	new file:   cymbal2.MP3 	new file:   cymbal3.wav 	new file:   cymbal4.wav 	modified:   index.html

--- creative_3.js
+++ creative_3.js
@@ -5,4 +5,19 @@ app.controller('mainCtrl', function($scope) {
     audio.load();
     audio.play(); 
     };
+   $scope.cymbal2 = function(){
+    var audio = document.getElementById("cymbal2audio");
+    audio.load();
+    audio.play(); 
+    };
+   $scope.cymbal3 = function(){
+    var audio = document.getElementById("cymbal3audio");
+    audio.load();
+    audio.play(); 
+    };
+   $scope.cymbal4 = function(){
+    var audio = document.getElementById("cymbal4audio");
+    audio.load();
+    audio.play(); 
+    };
 });
