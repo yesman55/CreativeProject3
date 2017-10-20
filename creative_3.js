e4fb81239cbb994c7e4f0f6f913968f570bdb605
yesman55 <jsi072795@yahoo.com>
2017-10-19 18:37:01 -0600

    More stuff

--- creative_3.js
+++ creative_3.js
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
 var app = angular.module('myApp', []);
 app.controller('mainCtrl', function($scope) {
+
+    $scope.More = "https://en.wikipedia.org/wiki/Drum";
+
     $scope.cymbal1 = function(){
         var audio = document.getElementById("cymbal1audio");
         audio.load();
@@ -41,49 +43,15 @@ app.controller('mainCtrl', function($scope) {
         audio.load();
         audio.play();
     };
+    $scope.displayLike = function() {
+        var clicked;
+        if (showLike == false) {
+            showLike = true;
+            return showLike;
+        }
+        else if (showLike == true) {
+            showLike = false;
+            return showLike;
+        }
+    }
 });
-=======
-var app = angular.module('myApp', []);
-app.controller('mainCtrl', function($scope) {
-    $scope.cymbal1 = function(){
-        var audio = document.getElementById("cymbal1audio");
-        audio.load();
-        audio.play(); 
-    };
-    $scope.cymbal2 = function(){
-        var audio = document.getElementById("cymbal2audio");
-        audio.load();
-        audio.play(); 
-    };
-    $scope.cymbal3 = function(){
-        var audio = document.getElementById("cymbal3audio");
-        audio.load();
-        audio.play(); 
-    };
-    $scope.cymbal4 = function(){
-        var audio = document.getElementById("cymbal4audio");
-        audio.load();
-        audio.play(); 
-    };
-    $scope.set1 = function() {
-        var audio = document.getElementById("set1audio");
-        audio.load();
-        audio.play();
-    };
-    $scope.set2 = function() {
-        var audio = document.getElementById("set2audio");
-        audio.load();
-        audio.play();
-    };
-    $scope.set4 = function() {
-        var audio = document.getElementById("set4audio");
-        audio.load();
-        audio.play();
-    };
-    $scope.bass1 = function() {
-        var audio = document.getElementById("bassAudio");
-        audio.load();
-        audio.play();
-    };
-});
->>>>>>> f844f834f2f092ffca5eb638837c14923faf6470
